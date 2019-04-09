82259d4af64e2280f2fdb29befaac48dd55a75a1
Matt Hedderly <mhedde200@caledonian.ac.uk>
2019-04-09 12:04:38 +0100

    added sample.cs and updated relevant fields for DTO

---
+++ S1520400_CPD_B/Models/Sample.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace S1520400_CPD_B.Models
+{
+
+    //This is the data transfer object class that is sent in the Rest requests/responses.
+    public class Sample
+    {
+        ///<summary>
+        /// Sample ID
+        /// </summary>
+        [Key]
+        public string SampleID { get; set; }
+
+        ///<summary>
+        /// Title of the sample
+        /// </summary>
+        public string Title { get; set; }
+
+        ///<summary>
+        /// Name of the Artist
+        /// </summary>
+        public string Artist { get; set; }
+
+        ///<summary>
+        /// creation date/time of entity
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+
+        ///<summary>
+        /// Name of uploaded blob in blob storage
+        /// </summary>
+        public string Mp3Blob { get; set; }
+
+        ///<summary>
+        /// Name of sample blob in blob storage
+        /// </summary>
+        public string SampleMp3Blob { get; set; }
+
+        ///<summary>
+        /// Web service resource URL of mp3 sample
+        /// </summary>
+        public string SampleMp3Url { get; set; }
+
+        ///<summary>
+        /// creation date/time of sample blob
+        /// </summary>
+        public DateTime SampleDate { get; set; }
+
+    }
+}
---
+++ S1520400_CPD_B/Models/SampleEntity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace S1520400_CPD_B.Models
+{
+    public class SampleEntity
+    {
+    }
+}
