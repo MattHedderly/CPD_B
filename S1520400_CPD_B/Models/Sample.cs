aed27429cdebbb9e577b6985dbe3c9357bb5ee83
Matt Hedderly <mhedde200@caledonian.ac.uk>
2019-04-09 12:33:27 +0100

    changed the DTO to reflect only the required properties as per the spec Title, mp3URL, artist and sampleID

--- S1520400_CPD_B/Models/Sample.cs
+++ S1520400_CPD_B/Models/Sample.cs
@@ -24,29 +24,9 @@ namespace S1520400_CPD_B.Models
         public string Artist { get; set; }
 
         ///<summary>
-        /// creation date/time of entity
-        /// </summary>
-        public DateTime CreatedDate { get; set; }
-
-        ///<summary>
-        /// Name of uploaded blob in blob storage
-        /// </summary>
-        public string Mp3Blob { get; set; }
-
-        ///<summary>
-        /// Name of sample blob in blob storage
-        /// </summary>
-        public string SampleMp3Blob { get; set; }
-
-        ///<summary>
         /// Web service resource URL of mp3 sample
         /// </summary>
         public string SampleMp3Url { get; set; }
-
-        ///<summary>
-        /// creation date/time of sample blob
-        /// </summary>
-        public DateTime SampleDate { get; set; }
 
     }
 }
--- S1520400_CPD_B/Models/SampleEntity.cs
+++ S1520400_CPD_B/Models/SampleEntity.cs
@@ -1,11 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿// Entity class for Azure table
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
 
 namespace S1520400_CPD_B.Models
 {
-    public class SampleEntity
+    public class SampleEntity : TableEntity
     {
+        public string SampleID { get; set; }
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public string Mp3Blob { get; set; }
+        public string SampleMp3Blob { get; set; }
+        public string SampleMp3Url { get; set; }
+        public DateTime SampleDate { get; set; }
+
+        public SampleEntity(string partitionKey, string sampleID)
+        {
+            PartitionKey = partitionKey;
+            RowKey = sampleID;
+        }
+
+        public SampleEntity() { }
+
     }
 }
