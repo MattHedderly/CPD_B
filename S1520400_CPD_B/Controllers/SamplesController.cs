180bd5f4cf030062956299633ab93c57d0859c9b
Matt Hedderly <mhedde200@caledonian.ac.uk>
2019-04-09 14:34:59 +0100

    created controller for REST api

---
+++ S1520400_CPD_B/Controllers/SamplesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Table;
+using System.Configuration;
+using System.Web.Http;
+
+
+// controller for the REST API
+namespace S1520400_CPD_B.Controllers
+{
+    public class SamplesController : ApiController
+    {
+        private const string partitionName = "Samples_Partition_1";
+
+        private CloudStorageAccount storageAccount;
+        private CloudTableClient tableClient;
+        private CloudTable table;
+
+        public SamplesController()
+        {
+            storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ToString());
+            tableClient = storageAccount.CreateCloudTableClient();
+            table = tableClient.GetTableReference("Samples");
+        }
+
+    }
+}
