7d2bb0d42cfbc56b9506ca01e9d5bb438d2dbb82
Matt Hedderly <mhedde200@caledonian.ac.uk>
2019-04-09 15:23:04 +0100

    api set up like lab 4, need to handle blob deletes

--- S1520400_CPD_B/Controllers/SamplesController.cs
+++ S1520400_CPD_B/Controllers/SamplesController.cs
@@ -1,10 +1,17 @@
 ﻿using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Table;
+using S1520400_CPD_B.Models;
+using System;
+using System.Collections.Generic;
 using System.Configuration;
+using System.Linq;
+using System.Net;
 using System.Web.Http;
+using System.Web.Http.Description;
 
 
 // controller for the REST API
+
 namespace S1520400_CPD_B.Controllers
 {
     public class SamplesController : ApiController
@@ -20,6 +27,173 @@ namespace S1520400_CPD_B.Controllers
             storageAccount = CloudStorageAccount.Parse(ConfigurationManager.ConnectionStrings["AzureWebJobsStorage"].ToString());
             tableClient = storageAccount.CreateCloudTableClient();
             table = tableClient.GetTableReference("Samples");
+        }
+
+        /// <summary>
+        /// Get all samples
+        /// </summary>
+        /// <returns></returns>
+        /// GET: api/Samples
+        public IEnumerable<Sample> get()
+        {
+            TableQuery<SampleEntity> query = new TableQuery<SampleEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionName));
+            List<SampleEntity> entityList = new List<SampleEntity>(table.ExecuteQuery(query));
+
+            //create a list of samples from the SampleEntity with a 1:1 object realtion
+            IEnumerable<Sample> sampleList = from e in entityList
+                                             select new Sample()
+                                             {
+                                                 SampleID = e.RowKey,
+                                                 Title = e.Title,
+                                                 Artist = e.Artist,
+                                                 SampleMp3Url = e.SampleMp3Url
+                                             };
+            return sampleList;
+
+        }
+
+        // GET: api/Samples/5
+        /// <Summary>
+        /// Get a sample
+        /// </Summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult GetSample(string id)
+        {
+            //create a retrueve operation that that takes a sample entity
+            TableOperation getOperation = TableOperation.Retrieve<SampleEntity>(partitionName, id);
+
+            // Execute the retrieve operation.
+            TableResult getOperationResult = table.Execute(getOperation);
+
+            // generate the response including the DTO
+            if (getOperationResult.Result == null) return NotFound();
+            else
+            {
+                SampleEntity sampleEntity = (SampleEntity)getOperationResult.Result;
+                Sample p = new Sample()
+                {
+                    SampleID = sampleEntity.RowKey,
+                    Title = sampleEntity.Title,
+                    Artist = sampleEntity.Artist,
+                    SampleMp3Url = sampleEntity.SampleMp3Url
+                };
+                return Ok(p);
+            }
+        }
+
+        // POST: api/Samples (should be done with webjob)
+        /// <summary>
+        /// Create a new sample
+        /// </summary>
+        /// <param name="Sample"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult PostSample(Sample sample)
+        {
+            SampleEntity sampleEntity = new SampleEntity()
+            {
+                RowKey = getNewMaxRowKeyValue(),
+                PartitionKey = partitionName,
+                Title = sample.Title,
+                Artist = sample.Artist,
+                SampleMp3Url = sample.SampleMp3Url,
+                SampleDate = null,
+                CreatedDate = DateTime.Now,
+                Mp3Blob = null,
+                SampleMp3Blob = null
+            };
+
+            //create the operation to insert into the table
+            var insertOperation = TableOperation.Insert(sampleEntity);
+
+            //execute it
+            table.Execute(insertOperation);
+
+            return CreatedAtRoute("DefaultApi", new { id = sampleEntity.RowKey }, sampleEntity);
+        }
+
+        // PUT: api/Samples/5
+        /// <summary>
+        /// Update a sample
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="sample"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutSample(string id, Sample sample)
+        {
+            if (id != sample.SampleID)
+            {
+                return BadRequest();
+            }
+
+            // Create a retrieve operation that takes a product entity.
+            TableOperation retrieveOperation = TableOperation.Retrieve<SampleEntity>(partitionName, id);
+
+            // Execute the operation.
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+
+            // Assign the result to a ProductEntity object.
+            SampleEntity updateEntity = (SampleEntity)retrievedResult.Result;
+
+            updateEntity.Title = sample.Title;
+            updateEntity.Artist = sample.Artist;
+            updateEntity.SampleMp3Url = sample.SampleMp3Url;
+
+            // Create the TableOperation that inserts the product entity.
+            var updateOperation = TableOperation.InsertOrReplace(updateEntity);
+
+            // Execute the insert operation.
+            table.Execute(updateOperation);
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // DELETE: api/Samples/5
+        /// <summary>
+        /// Delete a sample
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [ResponseType(typeof(Sample))]
+        public IHttpActionResult DeleteProduct(string id)
+        {
+            // Create a retrieve operation that takes a product entity.
+            TableOperation retrieveOperation = TableOperation.Retrieve<SampleEntity>(partitionName, id);
+
+            // Execute the retrieve operation.
+            TableResult retrievedResult = table.Execute(retrieveOperation);
+            if (retrievedResult.Result == null) return NotFound();
+            else
+            {
+                SampleEntity deleteEntity = (SampleEntity)retrievedResult.Result;
+                TableOperation deleteOperation = TableOperation.Delete(deleteEntity);
+
+                // Execute the operation.
+                table.Execute(deleteOperation);
+
+                return Ok(retrievedResult.Result);
+            }
+        }
+
+
+
+
+        //method for generating the rowkey for PUT using lab 4
+        private String getNewMaxRowKeyValue()
+        {
+            TableQuery<SampleEntity> query = new TableQuery<SampleEntity>().Where(TableQuery.GenerateFilterCondition("PartitionKey", QueryComparisons.Equal, partitionName));
+
+            int maxRowKeyValue = 0;
+            foreach (SampleEntity entity in table.ExecuteQuery(query))
+            {
+                int entityRowKeyValue = Int32.Parse(entity.RowKey);
+                if (entityRowKeyValue > maxRowKeyValue) maxRowKeyValue = entityRowKeyValue;
+            }
+            maxRowKeyValue++;
+            return maxRowKeyValue.ToString();
         }
 
     }
